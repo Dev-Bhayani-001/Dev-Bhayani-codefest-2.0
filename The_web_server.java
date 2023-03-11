0b9eaa5f94ec2426e3d795f99edf5ffdb872b331
Dev-Bhayani-001 <125212099+Dev-Bhayani-001@users.noreply.github.com>
2023-03-11 08:07:17 +0530

    Add files via upload

---
+++ The_web_server.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+//Dev Bhayani op in the chat 
+public class The_web_server {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][]website=new int[n][3];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                website[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(website[i][0]>website[j][0])
+                {
+                    int temp=website[i][0];
+                    website[i][0]=website[j][0];
+                    website[j][0]=temp;
+                    temp=website[i][1];
+                    website[i][1]=website[j][1];
+                    website[j][1]=temp;
+                    temp=website[i][2];
+                    website[i][2]=website[j][2];
+                    website[j][2]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(website[i][0]==website[j][0]&&website[i][1]>website[j][1])
+                {
+                    int temp=website[i][0];
+                    website[i][0]=website[j][0];
+                    website[j][0]=temp;
+                    temp=website[i][1];
+                    website[i][1]=website[j][1];
+                    website[j][1]=temp;
+                    temp=website[i][2];
+                    website[i][2]=website[j][2];
+                    website[j][2]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(website[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
